8f23f962fe87098173c402dec04e95bbb7d04ded
Fynnian Brosius <fynnian.brosius@gmx.de>
2024-05-13 11:17:56 +0200

    stage 3

--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,19 @@ import * as net from 'net';
 const server: net.Server = net.createServer();
 
 server.on('connection', (socket: net.Socket) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
-    socket.end();
+    socket.on('data', (data: Buffer) => {
+        const content = data.toString();
+        const [startLine] = content.split('\r\n');
+        const [_method, path, _httpVersion] = startLine.split(' ');
+
+        if (path === '/') {
+            socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        } else {
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+        }
+
+        socket.end();
+    });
 });
 
 // Uncomment this to pass the first stage
