6b3f960011ca12b6afe49d7bb0bb4e7ff5f8c017
Fynnian Brosius <fynnian.brosius@gmx.de>
2024-05-13 14:04:28 +0200

    stage http-compression:3

--- app/http-builder.ts
+++ app/http-builder.ts
@@ -2,21 +2,28 @@ import { STATUS_CODES } from 'http';
 
 interface HttpResponsePayload {
     statusCode: number;
-    content: string;
+    content: string|Buffer;
     contentType: string;
     additionalHeaders?: Record<string, string>;
 }
 
-export const buildHttpResponse = (payload: HttpResponsePayload) => {
-    const contentLength = (new TextEncoder().encode(payload.content)).length;
+export const buildHttpResponse = (payload: HttpResponsePayload): Buffer => {
+    const body = payload.content instanceof Buffer
+        ? payload.content
+        : Buffer.from(payload.content, 'utf-8');
+    const contentLength = body.toString().length;
     const headers = {
         'Content-Type': payload.contentType,
         'Content-Length': contentLength.toString(),
         ...payload.additionalHeaders,
     }
     const headersArray = Object.entries(headers).map(([key, value]) => `${key}: ${value}`);
-    return `HTTP/1.1 ${payload.statusCode} ${STATUS_CODES[payload.statusCode]}\r\n`
-        + `${headersArray.join('\r\n')}\r\n`
-        + '\r\n'
-        + `${payload.content}`;
+    return Buffer.concat([
+        Buffer.from(
+            `HTTP/1.1 ${payload.statusCode} ${STATUS_CODES[payload.statusCode]}\r\n`
+            + `${headersArray.join('\r\n')}\r\n`
+            + '\r\n'
+        ),
+        body
+    ]);
 }
